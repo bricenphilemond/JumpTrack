805be546a09eec61f5414ebb25b026861d45c0b3
Bricen Philemond <bgphilemond@seu.edu>
2026-04-23 21:05:47 -0400

    add main menu structure

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from tracker import save_jumps, load_jumps
 from jump import Jump 
+FILENAME = "jumps.csv"
+def main():
+    jumps = load_jumps(FILENAME)
+    while True:
+        print(" Welcome to JumpTrack!")
+        print("1. Add a jump")
+        print("2. View jumps")
+        print("3. View personal best")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
+    
+       
