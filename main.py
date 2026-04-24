5b6bb7f97245ed623a1128c8fbe5786dd14b5d51
Bricen Philemond <bgphilemond@seu.edu>
2026-04-23 22:12:58 -0400

    add main.py

--- main.py
+++ main.py
@@ -10,5 +10,27 @@ def main():
         print("3. View personal best")
         print("4. Exit")
         choice = input("Enter your choice: ")
-    
+        if choice == "1":
+            # Add a jump
+            input_date = input("Enter the date of the jump (YYYY-MM-DD):")
+            input_event = input("Enter the event(long jump or triple jump):")
+            input_distance1 = input("enter attempt 1 of the jump(or f for foul) in meters:")
+            input_distance2 = input("enter attempt 2 of the jump(or f for foul) in meters:")
+            input_distance3 = input("enter attempt 3 of the jump(or f for foul) in meters:")
+            input_distance4 = input("enter attempt 4 of the jump(or f for foul) in meters:")
+            attempts = [input_distance1, input_distance2, input_distance3, input_distance4]
+            jumps.append(Jump(input_date, input_event, attempts))
+            save_jumps(jumps, FILENAME)
+            print("Jump added successfully!")
+        elif choice == "2":
+            # view jumps
+            pass
+        elif choice == "3":
+            # view personal best
+            pass
+        elif choice == "4":
+            save_jumps(jumps, FILENAME)
+            print("Goodbye!")
+            break
+        
        
