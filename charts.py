f148aff4e74d8fb556b2b9ad36e7370c8d0b0095
Bricen Philemond <bgphilemond@seu.edu>
2026-04-26 00:15:57 -0400

    add charts.py

--- charts.py
+++ charts.py
@@ -4,3 +4,17 @@ def show_chart(jumps):
     long_jump_distances = []
     triple_jump_dates = []
     triple_jump_distances = []
+    for jump in jumps:
+        if jump.event == "long jump":
+            long_jump_dates.append(jump.date)
+            long_jump_distances.append(jump.best_attempt())
+        elif jump.event == "triple jump":
+            triple_jump_dates.append(jump.date)
+            triple_jump_distances.append(jump.best_attempt())
+    plt.plot(long_jump_dates, long_jump_distances, label="long jump")
+    plt.plot(triple_jump_dates, triple_jump_distances, label="triple jump")
+    plt.xlabel("date")
+    plt.ylabel("distance (m)")
+    plt.title("JumpTrack Overtime")
+    plt.legend()
+    plt.show()
