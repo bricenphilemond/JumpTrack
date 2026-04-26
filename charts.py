b0c2ffcf22449b61f26312c47d13da0fe37b15e0
Bricen Philemond <bgphilemond@seu.edu>
2026-04-26 00:38:50 -0400

    add charts.py

--- charts.py
+++ charts.py
@@ -1,3 +1,4 @@
+# I asked AI how to import and use matplotlib to create line charts and wrote this based on the response
 import matplotlib.pyplot as plt
 def show_chart(jumps):
     long_jump_dates = []
@@ -11,6 +12,7 @@ def show_chart(jumps):
         elif jump.event == "triple jump":
             triple_jump_dates.append(jump.date)
             triple_jump_distances.append(jump.best_attempt())
+    # I asked AI how to plot data with matplotlib and wrote this based on the response
     plt.plot(long_jump_dates, long_jump_distances, label="long jump")
     plt.plot(triple_jump_dates, triple_jump_distances, label="triple jump")
     plt.xlabel("date")
